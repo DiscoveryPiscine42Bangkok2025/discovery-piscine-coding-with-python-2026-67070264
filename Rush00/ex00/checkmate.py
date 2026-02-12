beeea15a23b5f4ffd5d32c716ae78119eaebea82
67070264 <67070264@kmitl.ac.th>
2026-02-13 02:35:36 +0700

    change

--- Rush00/ex00/checkmate.py
+++ Rush00/ex00/checkmate.py
@@ -7,17 +7,15 @@ def checkmate(board):
         rows = board.splitlines()
         size = len(rows)
 
-        # board must be square
         if size == 0 or any(len(row) != size for row in rows):
             print("Error")
             return
 
-        # find King
         king_pos = None
         for r in range(size):
             for c in range(size):
                 if rows[r][c] == 'K':
-                    if king_pos is not None:  # more than one king
+                    if king_pos is not None:  
                         print("Error")
                         return
                     king_pos = (r, c)
@@ -28,12 +26,9 @@ def checkmate(board):
 
         kr, kc = king_pos
 
-        # directions for rook & queen (straight)
         straight_dirs = [(-1,0),(1,0),(0,-1),(0,1)]
-        # directions for bishop & queen (diagonal)
         diag_dirs = [(-1,-1),(-1,1),(1,-1),(1,1)]
 
-        # check straight lines (R, Q)
         for dr, dc in straight_dirs:
             r, c = kr + dr, kc + dc
             while 0 <= r < size and 0 <= c < size:
@@ -46,7 +41,6 @@ def checkmate(board):
                 r += dr
                 c += dc
 
-        # check diagonals (B, Q)
         for dr, dc in diag_dirs:
             r, c = kr + dr, kc + dc
             while 0 <= r < size and 0 <= c < size:
@@ -59,7 +53,6 @@ def checkmate(board):
                 r += dr
                 c += dc
 
-        # check pawns (P)
         pawn_attacks = [(1,-1), (1,1)]
         for dr, dc in pawn_attacks:
             r, c = kr + dr, kc + dc
